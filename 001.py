4f2cb852b3b6924f2ecabbb90984ec29d86f0375
luohuiqi <cherryblossom1024@163.com>
2023-11-26 23:38:12 +0800

    init commit

--- 001.py
+++ 001.py
@@ -1,11 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-# Press the green button in the gutter to run the script.
+# hello world
 if __name__ == '__main__':
     print("hello world")
 
